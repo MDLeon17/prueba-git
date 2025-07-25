382674f4fafa5d62f30511cc5aca3324972329e3
MDLeon17 <fnandomendezdleon@gmail.com>
2025-07-25 09:30:27 -0600

    olis jeje

--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
 print("print('Hello, World!')")
+print ("Hello, World!")
+a = input("hola")
+if a == "hola":
+    print("Hola, mundo!")
+else:
+    print("No has escrito 'hola'")
+print("Fin del programa")
